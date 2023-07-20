a82edf2563f8ca1634ecb399e3f318b330505757
Yang Hyemin <asitwas729@gmail.com>
2023-07-20 13:33:06 +0900

    first commit

--- src/main/java/com/example/shoppingmall/config/PasswordEncoderConfig.java
+++ src/main/java/com/example/shoppingmall/config/PasswordEncoderConfig.java
@@ -6,9 +6,7 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class PasswordEncoderConfig {
-
-    @Bean
+public class PasswordEncoderConfig { @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
