f013226d5131ba23f87e3ca498859e2ef973d049
NERO <nerostarin@naver.com>
2023-08-04 15:47:00 +0900

    1.프런트와 상호작용하는 코드에 @RequestParam 추가
    2.상품등록 추가

--- src/main/java/com/example/shoppingmall/controller/ProductController.java
+++ src/main/java/com/example/shoppingmall/controller/ProductController.java
@@ -3,13 +3,13 @@ package com.example.shoppingmall.controller;
 import com.example.shoppingmall.entity.Product;
 import com.example.shoppingmall.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -21,50 +21,68 @@ public class ProductController {
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
-
-    @GetMapping("/listProduct")
+    
+    @GetMapping("/listProduct")//상품 전체출력
     public List<Product> SelectAllProduct()
     {
         List<Product> selectProduct = productService.selectAllProduct();
         System.out.println("컨트롤러에서"+selectProduct);
         return selectProduct;
     }
-    @GetMapping("/searchProduct")
-    public List<Product> SearchProduct(HttpServletRequest request)
+    @GetMapping("/searchProduct")//상품이름 검색
+    public List<Product> SearchProduct(HttpServletRequest request, @RequestParam String productName)
     {
-        //이게 웹페이지에서 받아오는거
-        //String productName = request.getParameter("productName");
+        String searchProductName = request.getParameter("productName");
 
-        //이건 테스트해볼려고 하는거
-        String a = "옷";
-        List<Product> searchProduct = productService.searchProduct(a);
+        List<Product> searchProduct = productService.searchProduct(searchProductName);
         System.out.println("컨트롤러에서"+searchProduct);
         return searchProduct;
     }
-    @GetMapping("/priceSort")
-    public List<Product> priceSort(HttpServletRequest request)
+    @GetMapping("/priceSort")//가격 오름차순, 내림차순 정렬
+    public List<Product> priceSort(HttpServletRequest request, @RequestParam String sort)
     {
-        //이게 웹페이지에서 받아오는거
-        //String sortPrice = request.getParameter("sort");
+        String sortPrice = request.getParameter("sort");
 
-        //이건 테스트해볼려고 하는거
-        String a = "내림차순";
-        List<Product> priceSort = productService.priceSort(a);
+        List<Product> priceSort = productService.priceSort(sortPrice);
         System.out.println("컨트롤러에서"+priceSort);
         return priceSort;
     }
 
-    @GetMapping("/searchCategory")
-    public List<Product> searchCategory(HttpServletRequest request)
+    @GetMapping("/searchCategory")//카테고리 검색
+    public List<Product> searchCategory(HttpServletRequest request, @RequestParam String category)
     {
-        //이게 웹페이지에서 받아오는거
-        //String category = request.getParameter("category");
+        String categoryName = request.getParameter("category");
 
-        //이건 테스트해볼려고 하는거
-        String a = "모자";
-        List<Product> searchCategory = productService.searchCategory(a);
+        List<Product> searchCategory = productService.searchCategory(categoryName);
         System.out.println("컨트롤러에서"+searchCategory);
         return searchCategory;
     }
+
+    @GetMapping("/insertProduct")//상품 등록
+    public ResponseEntity<Product> insertProduct(HttpServletRequest request, @RequestParam String category, @RequestParam String productName, @RequestParam int price,
+                                        @RequestParam int stock, @RequestParam int status, @RequestParam int cellCount, @RequestParam String spec,
+                                        @RequestParam String content, @RequestParam String url)
+    {
+        String ins_category = request.getParameter("category");
+        String ins_productName = request.getParameter("productName");
+        int ins_price = Integer.parseInt(request.getParameter("price"));
+        int ins_stock = Integer.parseInt(request.getParameter("stock"));
+        int ins_status = Integer.parseInt(request.getParameter("status"));
+        int ins_cellCount = Integer.parseInt(request.getParameter("cellCount"));
+        String ins_spec = request.getParameter("spec");
+        String ins_content = request.getParameter("content");
+        String ins_url = request.getParameter("url");
+        //시간
+        LocalDateTime beforeDate = LocalDateTime.now();
+        DateTimeFormatter afterDate = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String ins_createAt = beforeDate.format(afterDate);
+        String ins_updateAt = beforeDate.format(afterDate);
+
+       Product insertProduct = productService.insertProduct(ins_category, ins_productName, ins_price, ins_stock,
+                ins_status, ins_cellCount, ins_spec, ins_content, ins_url, ins_createAt, ins_updateAt).getBody();
+
+        return ResponseEntity.ok(insertProduct);
+    }
+
 
 }
--- src/main/java/com/example/shoppingmall/service/impl/ProductServiceImpl.java
+++ src/main/java/com/example/shoppingmall/service/impl/ProductServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.shoppingmall.entity.Product;
 import com.example.shoppingmall.repository.ProductRepository;
 import com.example.shoppingmall.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -50,6 +51,29 @@ public class ProductServiceImpl implements ProductService {
         List<Product> searchCategory = productRepository.findBycategory(category);
         System.out.println("서비스에서"+searchCategory);
         return searchCategory;
+    }
+
+    @Override
+    public ResponseEntity<Product> insertProduct(String ins_category, String ins_productName, int ins_price, int ins_stock,
+                                                 int ins_status, int ins_cellCount, String ins_spec, String ins_content,
+                                                 String ins_url, String ins_createAt, String ins_updateAt)
+    {
+        Product product = new Product();
+        product.setCategory(ins_category);
+        product.setProductName(ins_productName);
+        product.setPrice(ins_price);
+        product.setStock(ins_stock);
+        product.setStatus(ins_status);
+        product.setCellCount(ins_cellCount);
+        product.setSpec(ins_spec);
+        product.setContent(ins_content);
+        product.setUrl(ins_url);
+        product.setCreatedAt(ins_createAt);
+        product.setUpdatedAt(ins_updateAt);
+
+        Product insertProduct = productRepository.save(product);
+
+        return ResponseEntity.ok(insertProduct);
     }
 
 }
