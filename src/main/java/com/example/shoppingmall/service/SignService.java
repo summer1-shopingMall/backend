93b49f4a8d7b0e832df49f786f78032526f88dd3
NERO <nerostarin@naver.com>
2023-08-11 19:00:51 +0900

    1.판매자 엔티티 생성 2.판매자 회원가입 및 로그인 추가

--- src/main/java/com/example/shoppingmall/service/SignService.java
+++ src/main/java/com/example/shoppingmall/service/SignService.java
@@ -2,11 +2,23 @@ package com.example.shoppingmall.service;
 
 import com.example.shoppingmall.dto.SignInResultDto;
 import com.example.shoppingmall.dto.SignUpResultDto;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface SignService {
 
   SignUpResultDto UserSignUp(String userId, String password, String userName, String email, String phone);
 
+  SignUpResultDto SellerSignUp( String sellerID,  String sellerPw,
+                                String sellerName,  String phoneNumber,
+                                String companyNumber,  String boss,
+                                String companyName,  String address1,
+                                String address2,  String address3,
+                                String bank,  String receiveName,
+                                String bankAddress);
+
   SignInResultDto signIn(String id, String password) throws RuntimeException;
+
+  SignInResultDto SellerSignIn(String id, String password) throws RuntimeException;
+
 
 }
